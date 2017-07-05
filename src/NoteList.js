2a7fbafb8fa09f31529afa5da75b40d193b7f4a8
Ryan Burkholder <rcburkholder@anderson.edu>
2017-07-05 15:58:10 -0400

    Dynamically render notes from an object

--- src/NoteList.js
+++ src/NoteList.js
@@ -4,24 +4,28 @@ import './NoteList.css'
 import Note from './Note'
 
 const NoteList = () => {
-  const notes = [
-    {
+  const notes = {
+    'note-4': {
       id: 'note-4',
       title: 'My fancy note',
       body: 'Oh so fancy, I do declare',
     },
-    {
+    'note-5': {
       id: 'note-5',
       title: 'Another one',
       body: 'Also very fancy',
     },
+  }
 
-  ]
+  // array with note-4 and note-5 in it
+  const noteIds = Object.keys(notes)
+
+
     return(
         <div className="NoteList">
           <h3>Notes</h3>
           <ul id="notes">
-            {notes.map((note) => <Note note={note} />)}
+            {noteIds.map((noteId) => <Note note={notes[noteId]} />)}
           </ul>
         </div>
     )
