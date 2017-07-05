fb469e80ed38781a8462a13066d997e5d67814f4
Ryan Burkholder <rcburkholder@anderson.edu>
2017-07-05 15:31:42 -0400

    Style Sidebar, NoteList, and NoteForm

---
+++ src/NoteForm.js
@@ -0,0 +1,29 @@
+import React, { Component } from 'react'
+
+import './NoteForm.css'
+
+class NoteForm extends Component {
+    render() {
+        return (
+        <div className="NoteForm">
+          <div className="form-actions">
+            <button type="button">
+              <i className="fa fa-trash-o"></i>
+            </button>
+          </div>
+          <form>
+            <p>
+              <input
+                type="text"
+                name="title"
+                placeholder="Title your note"
+               />
+            </p>
+            <textarea name="body"></textarea>
+          </form>
+        </div>
+        )
+    }
+}
+
+export default NoteForm
